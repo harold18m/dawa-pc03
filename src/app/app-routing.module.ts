a9ae57449b40b929902d7d6057e2ca100f14230f
harold18m <harold.medrano@tecsup.edu.pe>
2024-05-30 18:03:56 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { Pagina1Component } from './components/pagina1/pagina1.component';
 import { Pagina2Component } from './components/pagina2/pagina2.component';
 import { HomeComponent } from './components/home/home.component';
+import { Pagina3Component } from './components/pagina3/pagina3.component';
+import { Pagina4Component } from './components/pagina4/pagina4.component';
+import { Pagina5Component } from './components/pagina5/pagina5.component';
 
 const routes: Routes = [
   { path: '', redirectTo : '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'pagina1', component: Pagina1Component },
-  { path: 'pagina2', component: Pagina2Component }
+  { path: 'pagina2', component: Pagina2Component },
+  { path: 'pagina3', component: Pagina3Component },
+  { path: 'pagina4', component: Pagina4Component },
+  { path: 'pagina5', component: Pagina5Component },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HomeComponent } from './components/home/home.component';
 import { Pagina1Component } from './components/pagina1/pagina1.component';
 import { Pagina2Component } from './components/pagina2/pagina2.component';
 import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Pagina3Component } from './components/pagina3/pagina3.component';
+import { Pagina4Component } from './components/pagina4/pagina4.component';
+import { Pagina5Component } from './components/pagina5/pagina5.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { NavmenuComponent } from './components/navmenu/navmenu.component';
     HomeComponent,
     Pagina1Component,
     Pagina2Component,
-    NavmenuComponent
+    NavmenuComponent,
+    FooterComponent,
+    Pagina3Component,
+    Pagina4Component,
+    Pagina5Component
   ],
   imports: [
     BrowserModule,
