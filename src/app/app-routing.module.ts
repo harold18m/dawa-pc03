f4f55698ad3fa62e78a3bf60c34cc4786cd5627d
harold18m <harold.medrano@tecsup.edu.pe>
2024-05-29 22:22:46 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Pagina1Component } from './components/pagina1/pagina1.component';
+import { Pagina2Component } from './components/pagina2/pagina2.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo : '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'pagina1', component: Pagina1Component },
+  { path: 'pagina2', component: Pagina2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
